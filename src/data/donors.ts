fcaf09785ae3f741e5bc978396501446a566cc0f
XiaoYouChR <xiaoyouchr@outlook.com>
2026-08-20 21:21:03 +0800

    docs: add donors

--- src/data/donors.ts
+++ src/data/donors.ts
@@ -10,6 +10,7 @@ export interface Donor {
 }
 
 export const donors: Donor[] = [
+  { name: '', amount: 10, at: '2026-08-20', channel: 'wechat', anonymous: true},
   { name: '缘', amount: 50, at: '2026-08-20', channel: 'wechat'},
   { name: '白板', amount: NaN, at: '2026-08-14', channel: 'wechat', message: '原道耳机: 早知道, 还是原道'},
   { name: '', amount: 100, at: '2026-08-14', channel: 'alipay', anonymous: true},
